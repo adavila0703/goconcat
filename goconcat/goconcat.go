d96cdd6e50bb246f2d9b1256ca4947fe8de52a3b
Angel <adavila0703@gmail.com>
2021-12-25 00:27:16 -0600

    working on getting files from dir

--- goconcat/goconcat.go
+++ goconcat/goconcat.go
@@ -1,10 +1,69 @@
 package goconcat
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
-	"mockconcat/flags"
 )
 
-func Goconcat() {
-	ioutil.ReadDir(*flags.Path)
+var (
+	ErrReadingDirectories = errors.New("error reading the directories")
+)
+
+func Goconcat() error {
+	fileNames, err := getFileNames(".")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(fileNames)
+	return nil
+}
+
+func getFileNames(path string) ([]string, error) {
+	var fileNames []string
+	var directories []string
+
+	directPath := path
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, ErrReadingDirectories
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			fileNames = append(fileNames, file.Name())
+		} else {
+			directories = append(directories, directPath+"/"+file.Name())
+		}
+	}
+
+	for {
+
+		for index, directory := range directories {
+			files, err := ioutil.ReadDir(directory)
+			if err != nil {
+				return nil, ErrReadingDirectories
+			}
+
+			fmt.Println(directories, directory)
+			directories = append(directories[:index], directories[index+1:]...)
+			fmt.Println(directories, directory)
+
+			for _, file := range files {
+				if !file.IsDir() {
+					fileNames = append(fileNames, file.Name())
+				} else {
+					directories = append(directories, directPath+"/"+file.Name())
+				}
+			}
+		}
+
+		if len(directories) == 0 {
+			break
+		}
+	}
+
+	return fileNames, nil
 }
