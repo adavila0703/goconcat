f7365eca0baf7f8f103dddf8f46fee085bffd83e
Angel <adavila0703@gmail.com>
2022-05-04 22:46:31 -0500

    large update

--- cmd/goconcat/main.go
+++ cmd/goconcat/main.go
@@ -1,88 +1,138 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
+	"github.com/adavila0703/goconcat"
 	"github.com/adavila0703/goconcat/internal/utils"
 	"github.com/adavila0703/goconcat/pkg/concat"
-
-	"github.com/pkg/errors"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/urfave/cli/v2"
 )
 
 var (
 	app = &cli.App{
-		Name:      "goconcat",
+		Name: `
+		██████╗  ██████╗  ██████╗ ██████╗ ███╗   ██╗ ██████╗ █████╗ ████████╗
+		██╔════╝ ██╔═══██╗██╔════╝██╔═══██╗████╗  ██║██╔════╝██╔══██╗╚══██╔══╝
+		██║  ███╗██║   ██║██║     ██║   ██║██╔██╗ ██║██║     ███████║   ██║   
+		██║   ██║██║   ██║██║     ██║   ██║██║╚██╗██║██║     ██╔══██║   ██║   
+		╚██████╔╝╚██████╔╝╚██████╗╚██████╔╝██║ ╚████║╚██████╗██║  ██║   ██║   
+		 ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   
+																																					
+		`,
+		Usage:     "Concatenate your Go files!",
 		UsageText: "goconcat can be used to concatenate multiple go files.",
 		Flags:     []cli.Flag{},
 		Commands: []*cli.Command{
 			{
-				Name:    "mockery",
-				Usage:   `Using the mockery flag will automatically look for all files which have a "mock_" prefix and sort them to a mockery package.`,
-				Action:  mockery,
-				Aliases: []string{"m"},
+				Name: "simple",
+				Usage: `A simple concatenation of multiple Go files.
+					args: [root-path][ignored-directories][file-prefix][destination][delete-old-files]
+				`,
+				UsageText: `
+				args details:
+				root-path: the directory you would like to start
+					example: "./home" - will only walk the home directory.
+				ignored-directories: directories you would like to ignore
+					example: "home,pkg,internal" - make sure you use a comma as a common delimite.
+				file-prefix: if you want to only point to files with a prefix
+					example: "mocks_" - will only look for files with a prefix of 'mocks_'.
+				destination: directory you would like your files to be moved to
+				delete-old-files: if you would like to delete your old files
+					exmple: true or false
+				`,
+				Action:    simpleConcat,
+				Aliases:   []string{"s"},
+				ArgsUsage: "[root-path][ignored-directories][file-prefix][destination][delete-old-files]",
+			},
+			{
+				Name: "json",
+				Usage: `Json allows you to write options in a json file. Use -h for help on the json structure.
+					args: [path-to-json]`,
+				UsageText: `
+				Json seetings example
+				{
+					"rootPath": ".",
+					"ignoredDirectories": ["file1", "file2"],
+					"filePrefix": ["mocks_", "mock"],
+					"destination": "./destination",
+					"deleteOldFiles": false,
+					"concatPkg": false
+				}
+				`,
+				Action:    jsonConcat,
+				Aliases:   []string{"j"},
+				ArgsUsage: "[path-to-json]",
 			},
 		},
 	}
 )
 
-func mockery(c *cli.Context) error {
-	options := utils.NewOptions(true, true)
-
-	filesToDelete, err := concat.GetFilePaths(
-		".",
-		[]utils.Directory{
-			utils.DirectoryGit,
-		},
-		[]utils.FileType{
-			utils.FileGo,
-		},
-		[]utils.PrefixType{
-			utils.PrefixGoconcat,
-		},
+func simpleConcat(c *cli.Context) error {
+	rootPath := c.Args().Get(0)
+	ignoredDirectories := utils.StringToType[utils.Directory](
+		strings.Split(c.Args().Get(1), ","),
 	)
+
+	filePrefix := utils.StringToType[utils.PrefixType](
+		strings.Split(c.Args().Get(2), ","),
+	)
+
+	destination := c.Args().Get(3)
+	deleteOldFiles, err := strconv.ParseBool(c.Args().Get(4))
 	if err != nil {
-		return errors.WithStack(err)
+		log.Fatal(concat.ErrBoolCouldNotBeParsed)
 	}
 
-	concat.DeleteFiles(filesToDelete)
-
-	filePaths, err := concat.GetFilePaths(
-		".",
-		[]utils.Directory{
-			utils.DirectoryGit,
-		},
-		[]utils.FileType{
-			utils.FileGo,
-		},
-		[]utils.PrefixType{
-			utils.PrefixMockery,
-		},
-	)
-	if err != nil {
-		return errors.WithStack(err)
+	fileTypes := []utils.FileType{
+		utils.FileGo,
 	}
 
-	if err := concat.GoConcat(
-		".",
-		[]utils.Directory{
-			utils.DirectoryGit,
-		},
-		[]utils.FileType{
-			utils.FileGo,
-		},
-		[]utils.PrefixType{
-			utils.PrefixMockery,
-		},
-		utils.DestinationMockery,
-		options,
-	); err != nil {
+	options := goconcat.NewOptions(
+		rootPath,
+		ignoredDirectories,
+		filePrefix,
+		destination,
+		deleteOldFiles,
+		false,
+		false,
+		fileTypes,
+	)
+
+	if err := goconcat.GoConcat(options); err != nil {
 		log.Fatal(err)
 	}
 
-	if len(filePaths) > 0 {
-		concat.DeleteFiles(filePaths)
+	return nil
+}
+
+func jsonConcat(c *cli.Context) error {
+	jsonFilePath := c.Args().Get(0)
+	if jsonFilePath == "" {
+		log.Fatal(concat.ErrNoFilePathForJson)
+	}
+
+	file, err := ioutil.ReadFile(jsonFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var options goconcat.Options
+
+	err = jsoniter.UnmarshalFromString(string(file), &options)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	options.FileType = append(options.FileType, utils.FileGo)
+
+	if err := goconcat.GoConcat(&options); err != nil {
+		log.Fatal(err)
 	}
 
 	return nil
